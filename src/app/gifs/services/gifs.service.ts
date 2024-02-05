11b76ee594018f7860aa059b7ed57fd58674042e
Mdperez19 <mdperezpriego@gmail.com>
2024-02-04 19:44:06 -0600

    Add API key and implement async searchTag method

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -6,6 +6,7 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  private  APIKEY: string = 'LMzumwFCNmM3DdT52MauGv7l8VcDI0ZG';
 
   constructor() { }
 
@@ -13,8 +14,27 @@ export class GifsService {
     return [...this._tagsHistory]; // return a copy of the array
   }
 
-  searchTag(tag: string): void {
-    this._tagsHistory.unshift(tag); // add the tag to the beginning of the array
+  async searchTag(tag: string): Promise<void> {
+    if (tag.trim().length === 0)
+      return;
+
+    this.organizeTagsHistory(tag);
+
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${this.APIKEY}&q=${tag}&limit=10`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      });
     console.log(this._tagsHistory);
+  }
+
+  organizeTagsHistory(tag: string) {
+    tag = tag.toLowerCase();
+    if(this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag !== tag);
+    }
+
+    this._tagsHistory.unshift(tag); // add the tag to the beginning of the array
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
   }
 }
